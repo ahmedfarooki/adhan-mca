300d8036e3908e85e3259686aba898d17faed056
Ahmed Farooki <ahmedfarooki@gmail.com>
2017-07-08 20:31:56 -0700

    Adding ecosystem configuration file

---
+++ ecosystem.config.js
@@ -0,0 +1,44 @@
+module.exports = {
+  /**
+   * Application configuration section
+   * http://pm2.keymetrics.io/docs/usage/application-declaration/
+   */
+  apps : [
+
+    // First application
+    {
+      name      : 'adhan-mca',
+      script    : 'index.js',
+      env: {},
+      env_production : {
+        NODE_ENV: 'production'
+      }
+    },
+  ],
+
+  /**
+   * Deployment section
+   * http://pm2.keymetrics.io/docs/usage/deployment/
+   */
+  deploy : {
+    production : {
+      user : 'pi',
+      host : '10.0.0.224',
+      ref  : 'origin/master',
+      repo : 'https://github.com/ahmedfarooki/adhan-mca.git',
+      path : '/home/pi/apps/adhan-mca',
+      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production'
+    },
+    dev : {
+      user : 'node',
+      host : '212.83.163.1',
+      ref  : 'origin/master',
+      repo : 'git@github.com:repo.git',
+      path : '/home/pi/apps/adhan-mca',
+      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env dev',
+      env  : {
+        NODE_ENV: 'dev'
+      }
+    }
+  }
+};
